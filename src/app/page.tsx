5535b072d78fc1cd8a67f8baaf961362d2f42a81
Ano Jumisa <anojumisa@gmx.de>
2024-12-04 23:13:09 +0800

    feature: Add mentor card draft for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import FeaturedMentors from "./components/FeaturedMentors";
 import FAQ from "./components/FAQ";
+
 
 export default function Home() {
   return (
     <>
+      <FeaturedMentors />
       <FAQ />
     </>
   );
