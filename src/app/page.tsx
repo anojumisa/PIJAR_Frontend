dbb602f5a98c8622fc4d8714692a98f0c39ad20f
ardy-jaya <ardy.henjaya@gmail.com>
2024-12-04 20:15:22 +0700

    feat: Navbar with search column, kategori and profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import FeaturedMentors from "@/components/FeaturedMentors";
 import FAQ from "../components/FAQ";
@@ -5,16 +6,20 @@ import Footer from "../components/Footer";
 import Banner from "./component/banner";
 import Topic from "./component/topic";
 import UpcomingSession from "./component/upcoming";
+import Homes from "./home";
+import Navbar from "./component/navbar";
 
 export default function Home() {
 	return (
 		<>
+     <Navbar />
 			<Banner />
 			<Topic />
 			<FeaturedMentors />
 			<UpcomingSession />
 			<FAQ />
 			<Footer />
+     
 		</>
 	);
 }
