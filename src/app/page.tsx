9e8e0ca28237a1675d9af549efb286f5fa281952
leonard800 <leonardabimanyu@gmail.com>
2024-12-05 14:58:35 +0700

    feat:additional features (banner, topic)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 import FeaturedMentors from "@/components/FeaturedMentors";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
+import Banner from "./component/banner";
+import Topic from "./component/topic";
 
 export default function Home() {
 	return (
 		<>
+			<Banner />
+			<Topic />
 			<FeaturedMentors />
 			<FAQ />
 			<Footer />
