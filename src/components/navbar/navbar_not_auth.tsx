4a49f89f2ed141eff9bfeea2f2bdd3dfe55706eb
Ano Jumisa <anojumisa@gmx.de>
2024-12-15 16:35:19 +0800

    fix: refactor navbar for api fetching

--- src/components/navbar/navbar_not_auth.tsx
+++ src/components/navbar/navbar_not_auth.tsx
@@ -1,10 +1,45 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { fetchCategories } from "@/utils/api";
+
+interface SubCategory {
+	category_id: number;
+	sub_category_name: string;
+}
+
+interface Category {
+	category_name: string;
+	id: number;
+	image_url: string;
+	sub_categories: SubCategory[];
+}
 
 const Navbar_not_auth: React.FC = () => {
+	const [categories, setCategories] = useState<Category[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const getCategories = async () => {
+			try {
+				const data = await fetchCategories();
+				setCategories(data);
+			} catch (error) {
+				setError("Error loading categories");
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		getCategories();
+	}, []);
+
+	// if (loading) return <div>Loading...</div>;
+	// if (error) return <div>{error}</div>;
+
 	return (
 		<nav className="bg-gradient-to-t from-sky-800 to-gray-900 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
 			<div className="flex-auto p-4">
@@ -35,14 +70,14 @@ const Navbar_not_auth: React.FC = () => {
 								transition
 								className="absolute right-0 z-10 mt-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
 							>
-								<div>
-									<MenuItem>
+								{categories.map((category) => (
+									<MenuItem key={category.id}>
 										<Menu
 											as="div"
 											className="relative inline-block text-left w-full"
 										>
 											<MenuButton className="inline-flex w-full justify-between px-3 py-1 text-sm font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100 my-1">
-												<span>Grafik Desain</span>
+												<span>{category.category_name}</span>
 												<ChevronRightIcon
 													aria-hidden="true"
 													className="h-5 w-5 text-gray-400"
@@ -52,128 +87,20 @@ const Navbar_not_auth: React.FC = () => {
 												transition
 												className="absolute left-full top-0 z-10 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
 											>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt1
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt1
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt2
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt3
-													</a>
-												</MenuItem>
+												{category.sub_categories.map((subCategory) => (
+													<MenuItem key={subCategory.category_id}>
+														<a
+															href="#"
+															className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+														>
+															{subCategory.sub_category_name}
+														</a>
+													</MenuItem>
+												))}
 											</MenuItems>
 										</Menu>
 									</MenuItem>
-									<MenuItem>
-										<Menu
-											as="div"
-											className="relative inline-block text-left w-full"
-										>
-											<MenuButton className="inline-flex w-full justify-between px-3 py-1 text-sm font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100 my-1">
-												<span>Grafik Desain</span>
-												<ChevronRightIcon
-													aria-hidden="true"
-													className="h- w-5 text-gray-400"
-												/>
-											</MenuButton>
-											<MenuItems
-												transition
-												className="absolute left-full top-0 z-10 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-											>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-3 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt1
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt2
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt3
-													</a>
-												</MenuItem>
-											</MenuItems>
-										</Menu>
-									</MenuItem>
-									<MenuItem>
-										<Menu
-											as="div"
-											className="relative inline-block text-left w-full"
-										>
-											<MenuButton className="inline-flex w-full justify-between px-3 py-1 text-sm font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100 my-1">
-												<span>Grafik Desain</span>
-												<ChevronRightIcon
-													aria-hidden="true"
-													className="h-5 w-5 text-gray-400"
-												/>
-											</MenuButton>
-											<MenuItems
-												transition
-												className="absolute left-full top-0 z-10 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-											>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt1
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt2
-													</a>
-												</MenuItem>
-												<MenuItem>
-													<a
-														href="#"
-														className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
-													>
-														Opt3
-													</a>
-												</MenuItem>
-											</MenuItems>
-										</Menu>
-									</MenuItem>
-								</div>
+								))}
 							</MenuItems>
 						</Menu>
 						<li>
