9adb6e30cd9554af6bac39fcc549dd706e96ed47
leonard800 <leonardabimanyu@gmail.com>
2024-12-16 14:31:53 +0700

    feat: init branch topic=details

---
+++ src/app/topic/[topicId]/components/ImageButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from "react";
+
+interface ImageButtonProps {
+    category_name: string;
+    image_url: string;
+}
+
+const ImageButton: React.FC = () => {
+    return(
+        <div></div>
+    )
+}
