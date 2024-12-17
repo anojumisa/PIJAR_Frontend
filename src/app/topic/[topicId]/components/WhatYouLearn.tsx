d466513b988b3483ce79b0639a024be4d26ad67c
leonard800 <leonardabimanyu@gmail.com>
2024-12-17 17:59:43 +0700

    feat: working on topicheader

--- src/app/topic/[topicId]/components/WhatYouLearn.tsx
+++ src/app/topic/[topicId]/components/WhatYouLearn.tsx
@@ -6,10 +6,10 @@ interface WhatYouLearnProps {
 };
   
 const WhatYouLearn: React.FC<WhatYouLearnProps> = ({ details }) => {
-    useState ();
 
     return (
-        <div className="flex flex-col">
+        <div className="text-xs md:text-sm lg:text-lg ">
+            <h1 className="font-bold font-caveat text-sm md:text-base lg:text-lg">Apa yang akan dipelajari:</h1>
             <p className="text-2xl font-bold">{details}</p>
         </div>
     );
