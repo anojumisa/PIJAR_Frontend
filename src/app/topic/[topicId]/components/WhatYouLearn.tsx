d6fd76b0f8a5634cb2bf26ef0bb4111224e0aa48
leonard800 <leonardabimanyu@gmail.com>
2024-12-18 13:48:50 +0700

    feat: added image button

--- src/app/topic/[topicId]/components/WhatYouLearn.tsx
+++ src/app/topic/[topicId]/components/WhatYouLearn.tsx
@@ -8,9 +8,9 @@ interface WhatYouLearnProps {
 const WhatYouLearn: React.FC<WhatYouLearnProps> = ({ details }) => {
 
     return (
-        <div className="mt-6 bg-neutral-300  p-4 rounded-lg shadow border border-neutral-200">
-            <h1 className="font-bold font-caveat text-sm md:text-base lg:text-lg">Apa yang akan dipelajari:</h1>
-            <p className="text-xl">{details}</p>
+        <div className="mt-6 bg-neutral-300 p-4 shadow border border-neutral-200">
+            <h1 className="font-bold font-caveat text-2xl md:text-base lg:text-lg">Apa yang akan dipelajari:</h1>
+            <p className="text-sm">{details}</p>
         </div>
     );
 };
--- src/app/topic/[topicId]/page.tsx
+++ src/app/topic/[topicId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import ImageButton from "./components/ImageButton";
 import TopicHeader from "./components/TopicHeader";
 import WhatYouLearn from "./components/WhatYouLearn";
 
@@ -8,7 +9,7 @@ const MOCK_TOPIC_DATA = {
     title: "Belajar Dasar Coding",
     short_description: "Belajar pemrograman dasar untuk pemula",
     rating: 4.,
-    details: "Belajar pemrograman dasar untuk pemula",
+    details: "Pada materi ini pelajar akan diperkenalkan dengan dasar-dasar pemgrograman. Topik yang akan dibahas pada materi ini akan berupa bagaimana cara membuat HTML sederhana serta pengenalan tentang repository Git.",
     link: "https://www.youtube.com/embed/iA8lLwmtKQM?list=PLZS-MHyEIRo59lUBwU-XHH7Ymmb04ffOY",
     image_url: "https://via.placeholder.com/150"
 }
@@ -16,6 +17,7 @@ const MOCK_TOPIC_DATA = {
 const TopicPage: React.FC = () => {
     return (
         <div className="container mx-auto px-4 py-8">
+            <ImageButton image_url={MOCK_TOPIC_DATA.image_url} title={MOCK_TOPIC_DATA.title} link={MOCK_TOPIC_DATA.link} />
             <TopicHeader title={MOCK_TOPIC_DATA.title} short_description={MOCK_TOPIC_DATA.short_description} rating={MOCK_TOPIC_DATA.rating} />
             <WhatYouLearn details={MOCK_TOPIC_DATA.details} />
         </div>
