84e1ed3846b8eae49113093c974d069cf26b0f24
leonard800 <leonardabimanyu@gmail.com>
2024-12-18 17:48:08 +0700

    feat: add star rating

--- src/app/topic/[topicId]/components/TopicHeader.tsx
+++ src/app/topic/[topicId]/components/TopicHeader.tsx
@@ -12,7 +12,27 @@ const TopicHeader: React.FC<TopicHeaderProps> = ({ title, short_description, rat
         <div className="p-4 bg-neutral-300 shadow border border-neutral-200">
             <h1 className="font-bold font-caveat text-2xl">{title}</h1>
             <p className="text-sm font-bold text-gray-600">{short_description}</p>
-            <p className="text-sm font-bold text-gray-600">Rating: {rating}/5</p>
+            <div className="text-sm font-bold text-yellow-600">
+                {[...Array(5)].map((star, index) => {
+                    const decimalRating = rating;
+                    const fullStar = index + 1;
+                    const isFullStar = decimalRating >= fullStar;
+                    const isPartialStar = decimalRating > index && decimalRating < fullStar;
+                    
+                    return (
+                    <span key={index} className="relative inline-block">
+                        {isFullStar && <span className="text-yellow-500">★</span>}
+                        {isPartialStar && (
+                        <div className="absolute top-0 left-0 overflow-hidden" style={{width: `${(decimalRating - index) * 100}%`}}>
+                            <span className="text-yellow-500">Ratings: </span>
+                        </div>
+                        )}
+                        {!isFullStar && !isPartialStar && <span className="text-gray-300">★</span>}
+                    </span>
+                    );
+                })}
+                <span className="ml-2">({rating}/5)</span>
+            </div>
         </div>
     )
 }
--- src/app/topic/[topicId]/page.tsx
+++ src/app/topic/[topicId]/page.tsx
@@ -8,7 +8,7 @@ const MOCK_TOPIC_DATA = {
     id: "123",
     title: "Belajar Dasar Coding",
     short_description: "Belajar pemrograman dasar untuk pemula",
-    rating: 4.,
+    rating: 4.8,
     details: "Pada materi ini pelajar akan diperkenalkan dengan dasar-dasar pemgrograman. Topik yang akan dibahas pada materi ini akan berupa bagaimana cara membuat HTML sederhana serta pengenalan tentang repository Git.",
     link: "https://www.youtube.com/embed/iA8lLwmtKQM?list=PLZS-MHyEIRo59lUBwU-XHH7Ymmb04ffOY",
     image_url: "https://via.placeholder.com/150"
