ebf0ad9b5e91331481a37137645f3f68725c7ed1
reyvan.irsandy <reyvan.irsandy@gopay.co.id>
2024-12-18 23:13:00 +0700

    fix: styling at minat

--- src/app/minat/page.tsx
+++ src/app/minat/page.tsx
@@ -45,18 +45,18 @@ const Selector: React.FC<SelectorProps> = ({ interests, onSelect, placeholder })
       </MenuButton>
       <MenuItems
         transition
-        className="absolute mt-1 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+        className="absolute z-10 mt-1 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
       >
         {interests.map((interest) => (
-          <MenuItem key={interest.id}>
-            <MenuButton className="inline-flex w-full justify-between px-3 py-1 text-sm font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100 my-1">
+          <MenuItem as="div" className="inline-flex w-full justify-between px-3 py-1 text-sm font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100 my-1" key={interest.id}>
+            {/* <MenuButton > */}
               <div
                 onClick={() => handleOnSelectItem(interest)}
                 className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
               >
                 {interest.category_name}
               </div>
-            </MenuButton>
+            {/* </MenuButton> */}
           </MenuItem>
         ))}
       </MenuItems>
