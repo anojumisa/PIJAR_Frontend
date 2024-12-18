66b210eb93a6d43a7ff005821c380e9b2595b461
leonard800 <leonardabimanyu@gmail.com>
2024-12-18 13:48:50 +0700

    feat: added image button

--- src/app/topic/[topicId]/components/ImageButton.tsx
+++ src/app/topic/[topicId]/components/ImageButton.tsx
@@ -2,12 +2,31 @@
 import React, { useState } from "react";
 
 interface ImageButtonProps {
-    category_name: string;
-    image_url: string;
+    image_url: string,
+    title: string,
+    link: string
 }
 
-const ImageButton: React.FC = () => {
+const ImageButton: React.FC<ImageButtonProps> = ({ image_url, title, link }) => {
     return(
-        <div></div>
+        <div className="mt-6 bg-neutral-300 p-4 shadow border border-neutral-200">
+            <img
+                src={image_url}
+                alt={title}
+                className="h-20 w-full object-cover rounded-lg"
+            />
+            <div className="mt-4">
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-medium text-sm rounded hover:bg-blue-600 transition duration-300"
+                >
+                    Join Now
+                </a>
+            </div>
+        </div>
     )
-}
+};
+
+export default ImageButton;
--- src/app/topic/[topicId]/components/TopicHeader.tsx
+++ src/app/topic/[topicId]/components/TopicHeader.tsx
@@ -9,10 +9,10 @@ interface TopicHeaderProps {
 
 const TopicHeader: React.FC<TopicHeaderProps> = ({ title, short_description, rating }) => {
     return(
-        <div className="mt-6 bg-neutral-300  p-4 rounded-lg shadow border border-neutral-200">
-            <h1 className="font-bold font-caveat text-sm md:text-base lg:text-lg">{title}</h1>
-            <p className="text-2xl font-bold text-gray-600">{short_description}</p>
-            <p className="text-2xl font-bold text-gray-600">Rating: {rating}/5</p>
+        <div className="mt-6 bg-neutral-300  p-4 shadow border border-neutral-200">
+            <h1 className="font-bold font-caveat text-2xl md:text-base lg:text-lg">{title}</h1>
+            <p className="text-sm font-bold text-gray-600">{short_description}</p>
+            <p className="text-sm font-bold text-gray-600">Rating: {rating}/5</p>
         </div>
     )
 }
