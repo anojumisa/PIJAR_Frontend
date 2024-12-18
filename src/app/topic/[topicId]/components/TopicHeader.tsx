ceb6e581555555ad121a871725f6e547518c9daf
leonard800 <leonardabimanyu@gmail.com>
2024-12-18 13:30:06 +0700

    feat: continue topic

--- src/app/topic/[topicId]/components/TopicHeader.tsx
+++ src/app/topic/[topicId]/components/TopicHeader.tsx
@@ -4,13 +4,15 @@ import React, { useState } from "react";
 interface TopicHeaderProps {
     title: string;
     short_description: string;
+    rating: number;
 };
 
-const TopicHeader: React.FC<TopicHeaderProps> = ({ title, short_description }) => {
+const TopicHeader: React.FC<TopicHeaderProps> = ({ title, short_description, rating }) => {
     return(
         <div className="mt-6 bg-neutral-300  p-4 rounded-lg shadow border border-neutral-200">
             <h1 className="font-bold font-caveat text-sm md:text-base lg:text-lg">{title}</h1>
             <p className="text-2xl font-bold text-gray-600">{short_description}</p>
+            <p className="text-2xl font-bold text-gray-600">Rating: {rating}/5</p>
         </div>
     )
 }
--- src/app/topic/[topicId]/components/WhatYouLearn.tsx
+++ src/app/topic/[topicId]/components/WhatYouLearn.tsx
@@ -8,9 +8,9 @@ interface WhatYouLearnProps {
 const WhatYouLearn: React.FC<WhatYouLearnProps> = ({ details }) => {
 
     return (
-        <div className="text-xs md:text-sm lg:text-lg ">
+        <div className="mt-6 bg-neutral-300  p-4 rounded-lg shadow border border-neutral-200">
             <h1 className="font-bold font-caveat text-sm md:text-base lg:text-lg">Apa yang akan dipelajari:</h1>
-            <p className="text-2xl font-bold">{details}</p>
+            <p className="text-xl">{details}</p>
         </div>
     );
 };
