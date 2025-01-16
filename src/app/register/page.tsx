81b3524b7499a35fa00da2e646b6bfeafac79863
ardy-jaya <ardy.henjaya@gmail.com>
2025-01-16 12:37:51 +0700

    Update page.tsx
    feat: enhance RegisterPage layout with responsive design and improved button styles

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -16,17 +16,20 @@ const RegisterPage: React.FC = () => {
     };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <h1 style={{ color: 'white', fontSize: '3rem', fontWeight: 'italic', textAlign: 'center', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                Ruang Belajar Anda Dimanapun dan Kapanpun
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-black text-3xl text-center font-bold shadow-lg bg-white bg-opacity-50 p-4 rounded">
+            Ruang Belajar Anda Dimanapun dan Kapanpun
             </h1>
-            <div style={{ marginTop: '20px' }}>
-                <button onClick={() => handleOptionClick('mentor')} style={{ marginRight: '10px', padding: '10px 20px', backgroundColor: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-                    Mentor
-                </button>
-                <button onClick={() => handleOptionClick('learner')} style={{ padding: '10px 20px', backgroundColor: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-                    Learner
-                </button>
+            
+            <div style={{ marginTop: '300px', display: 'flex' }}>
+            <button onClick={() => handleOptionClick('mentor')} className="mr-16 px-12 py-7 bg-green-600 border-2 rounded cursor-pointer text-5xl flex flex-col items-center">
+                <img alt="Robot" className="w-30 h-30 lg:w-48 lg:h-48 mb-4" src="/signin.png" />
+                <p>Hello, I am Student</p>
+            </button>
+            <button onClick={() => handleOptionClick('learner')} className="px-12 py-6 bg-yellow-600 border-2 rounded cursor-pointer text-5xl flex flex-col items-center">
+                <img alt="Robot" className="w-30 h-30 lg:w-48 lg:h-48 mb-4" src="/image.png" />
+                <p>Hello, I am Mentor</p>
+            </button>
             </div>
         </div>
     );
