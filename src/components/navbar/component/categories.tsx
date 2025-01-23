f9f7d57d1a2ea763d80901240e95400c73d9dc29
ardy-jaya <ardy.henjaya@gmail.com>
2025-01-23 22:29:12 +0700

    fix(navbar): improve categories menu layout for better responsiveness

--- src/components/navbar/component/categories.tsx
+++ src/components/navbar/component/categories.tsx
@@ -8,8 +8,8 @@ interface CategoriesProps {
 }
 
 export const Categories: React.FC<CategoriesProps> = ({ categories }) => (
-    <Menu as="div" className="relative inline-block pr-3">
-        <MenuButton className="inline-flex w-full justify-center  gap-x-1.5 rounded-md bg-white px-3 py-2.5  text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+    <Menu as="div" className="relative inline-block w-full sm:w-auto">
+        <MenuButton className="inline-flex w-full justify-between sm:justify-center gap-x-1.5 rounded-md bg-white px-3 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
             Kategori
             <ChevronDownIcon
                 className="-mr-1 h-5 w-5 text-gray-400"
@@ -18,7 +18,7 @@ export const Categories: React.FC<CategoriesProps> = ({ categories }) => (
         </MenuButton>
         <MenuItems
             transition
-            className="absolute right-0 z-10 mt-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+            className="absolute right-0 z-10 mt-1 w-full sm:w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
         >
             {categories.map((category) => (
                 <MenuItem key={category.id}>
